13548a4c42d853307e59c982a1602e3de71f108e
namdw <dnjsxodp@gmail.com>
2017-09-04 17:51:32 +0900

    updated base to buildnet version

--- base.py
+++ base.py
@@ -192,10 +192,201 @@ class NN():
 			self.W[-1-i] = self.W[-1-i] - n * (np.dot(np.transpose(A[-2-i])/np.prod(A[-2-i].shape), D[i]))
 			self.B[-1-i] = self.B[-1-i] - n * D[i]
 
-
-class NNb():
-
+class NNb(NN):
+	'''
+	Create a Custom Neural Network
+	input:
+	numX, numW, numY
+	numX(int) : number of inputs nodes of the network. Default 2
+	numW([int]) : list of number of weight for each hidden layer. [numW1, numW2,...,numWH]. Default [5]
+	numY(int) : number of outputs to the network. Default 2
+	'''
 	def __init__(self):
-		pass
+		## Default values
+		self.numX = 0
+		self.numW = []
+		self.numY = 0
+		self.epoch = 3
+		self.num_layer = 0
 
-	
+
+		## Initialize input, weights, and outputs of the network
+		self.X = []
+		self.W = []
+		self.B = []
+		self.Y = []
+		self.layers = []
+
+	def addLayer(self, layer):
+		if(layer.type=='input' and self.num_layer==0):
+			self.numX = layer.num_node
+		else:
+			self.numW.append(layer.num_node)
+			if(layer.weight=='xavier'):
+				print('weight : Xavier')
+				layer.value = np.random.normal(0, 1,(self.layers[-1].num_node, layer.num_node))
+				layer.B = np.random.normal(0, 1,(1, layer.num_node))
+			else:
+				print('weight : Random')
+				layer.value = layer.weight_scale*np.random.rand(self.layers[-1].num_node, layer.num_node)-layer.weight_scale/2.0
+				layer.B = layer.weight_scale*np.random.rand(1, layer.num_node)-layer.weight_scale/2.0
+			self.W.append(layer.value)
+			self.B.append(layer.B)
+
+		self.layers.append(layer)
+
+
+	'''
+	Sigmoid function for an array
+	X(float[]) : array of values to calculate sigmoid
+	'''	
+	# def sigmoid(self, X):
+	# 	result = np.zeros(X.shape)
+	# 	for i in range(len(X[0][:])):
+	# 		try:
+	# 			result[0][i] = 1 / (1+math.exp(-1*X[0][i])) # sigmoid function 
+	# 		except OverflowError:
+	# 			print("Over flow", X[0][i])
+	# 	return result
+
+	# def relu(self, X):
+	# 	result = np.zeros(X.shape)
+	# 	for i in range(len(X[0][:])):
+	# 		result[0][i] = max([0,X[0][i]])
+	# 	return result
+
+	# def relu2(self, X):
+	# 	result = X
+	# 	return result
+
+	# def lrelu(self, X):
+	# 	result = np.zeros(X.shape)
+	# 	for i in range(len(X[0][:])):
+	# 		result[0][i] = X[0][i] if X[0][i] > 0 else 0.001*X[0][i]
+	# 	return result
+
+	'''
+	NN forward propagation
+	X(float[]) : array of inputs to calculate the forward propagation
+	'''
+	def forward(self, X):
+		if(type(X)==type([])):
+			X = np.array([X])
+		a = X
+		for i in range(len(self.W)):
+			z = np.dot(a / np.prod(a.shape), self.W[i]) + self.B[i] # z = a*w + b
+			if (i==len(self.W)-1):
+				return z
+			else:
+				if(self.layers[i+1].func=='sigmoid'):
+					a = self.sigmoid(z) # a = sigma(a*w)
+				elif(self.layers[i+1].func=='relu'):
+					a = self.relu(z)
+				elif(self.layers[i+1].func=='reul2'):
+					a = self.relu2(z)
+				elif(self.layers[i+1].func=='lrelu'):
+					a = self.lrelu(z)
+				else:
+					a = self.relu(z)
+				# if(self.dropout<1.0):
+				# 	a = np.multiply(a, np.random.binomial([p for p in np.ones(a.shape)], self.dropout)[0] * (1.0/(1-self.dropout)))
+		return a
+
+	def train(self, X, Y, *arg):
+		if(type(X)==type([])):
+			X = np.array([X])
+			Y = np.array([Y])
+		if(len(arg)==1):
+			n = arg
+		else:
+			n = 1.0
+		A = [0]*(len(self.W)+1)
+		Z = [0]*len(self.W)
+		A[0] = X
+		for i in range(len(self.W)):
+			Z[i] = np.dot(A[i], self.W[i]) + self.B[i] # z = a*w + b
+			if (i==len(self.W)-1):
+				A[i+1] = Z[i]
+			else:
+				if(self.layers[i+1].func=='sigmoid'):
+					A[i+1] = self.sigmoid(Z[i]) # a = sigma(a*w)
+				elif(self.layers[i+1].func=='relu'):
+					A[i+1] = self.relu(Z[i])
+				elif(self.layers[i+1].func=='relu2'):
+					A[i+1] = self.relu2(Z[i])
+				elif(self.layers[i+1].func=='lrelu'):
+					A[i+1] = self.lrelu(Z[i])
+				else:
+					A[i+1] = self.relu(Z[i])
+				if(self.layers[i+1].dropout<1.0):
+					dropout_list = np.random.binomial([p for p in np.ones(A[i+1].shape)], self.layers[i+1].dropout)
+					if(np.sum(dropout_list)==0):
+						dropout_list[np.random.randint(len(dropout_list))] = 1
+					A[i+1] = np.multiply(A[i+1], dropout_list[0]) / np.sum(dropout_list) #* (1.0/(1-self.dropout)))
+		
+		D = [0]*(len(self.W)+1)
+		# D[0] = (A[-1]-Y) * A[-1]*(1-A[-1])
+		D[0] = (A[-1]-Y)
+		# D[0] = 0.5 * (A[-1]-Y)**2 / np.prod(A[-1].shape) * A[-1]*(1-A[-1])
+		np.seterr(all='raise')
+		for i in range(len(self.W)):
+			if (self.layers[i+1].func=='sigmoid'):
+				D[i+1] = np.multiply(np.transpose(np.dot(self.W[-i-1], np.transpose(D[i]))), np.multiply(A[-2-i],(1-A[-2-i])))
+			elif (self.layers[i+1].func=='relu'):
+				try:
+					D[i+1] = np.multiply(np.transpose(np.dot(self.W[-i-1], np.transpose(D[i]))), np.array([1 if element>0 else 0 for element in A[-2-i][0]]))
+				except:
+					print(np.transpose(np.dot(self.W[-i-1], np.transpose(D[i]))))
+			elif (self.layers[i+1].func=='relu2'):
+				D[i+1] = np.transpose(np.dot(self.W[-i-1], np.transpose(D[i])))
+			elif (self.layers[i+1].func=='lrelu'):
+				D[i+1] = np.multiply(np.transpose(np.dot(self.W[-i-1], np.transpose(D[i]))), np.array([1 if element>0 else 0.001 for element in A[-2-i][0]]))
+			else: # Default is ReLU
+				print('function note set')
+				D[i+1] = np.multiply(np.transpose(np.dot(self.W[-i-1], np.transpose(D[i]))), np.array([1 if element>0 else 0 for element in A[-2-i][0]]))
+			
+			# Update the parameters
+			dx = (np.dot(np.transpose(A[-2-i])/np.prod(A[-2-i].shape), D[i]))
+			if (self.layers[i+1].optimizer=='Vanilla'):
+				self.W[-1-i] = self.W[-1-i] - n * dx
+				self.B[-1-i] = self.B[-1-i] - n * D[i]
+			elif (self.layers[i+1].optimizer=='ADAM'):
+				self.layers[i+1].m = self.layers[i+1].beta1*self.layers[i+1].m + (1-self.layers[i+1].beta1)*dx
+				self.layers[i+1].v = self.layers[i+1].beta2*self.layers[i+1].v + (1-self.layers[i+1].beta2)*(dx**2)
+				self.W[-1-i] = self.W[-1-i] - n * self.layers[i+1].m / np.sqrt(v)+self.layers[i+1].eps
+				self.B[-1-i] = self.B[-1-i] - n * D[i]
+			else: # use Vanilla
+				self.W[-1-i] = self.W[-1-i] - n * dx
+				self.B[-1-i] = self.B[-1-i] - n * D[i]
+
+class Layer():
+	def __init__(self, type, num_node, **kargs):
+		self.type = type
+		self.num_node = num_node
+		self.value = None
+		self.B = None
+
+		self.beta1 = 0.9
+		self.beta2 = 0.999
+		self.eps = 1e-8
+		self.m = 0
+		self.v = 0
+		# if(self.type=='output' or self.type=='hidden'):
+		# 	self.value = np.zeros([prev_num_node, self.num_node])
+
+
+		self.func = 'relu'
+		self.dropout = 1.0
+		self.weight = 'Random'
+		self.weight_scale = 1.0
+		self.optimizer = 'Vanilla'
+
+		for name, value in kargs.items():
+			if(name=='func'):
+				self.func = value
+			elif(name=='dropout'):
+				self.dropout = float(value)
+			elif(name=='weight'):
+				self.weight = value
+			elif(name=='weight_scale'):
+				self.weight_scale = float(value)
